147da2cbf0902c14aa9f8a2421a4b95d536e5f80
GANG XU <GANG XU>
2016-12-12 17:02:31 +0800

    init

---
+++ src/main/java/com/cloume/radar/WxappApplication.java
@@ -0,0 +1,12 @@
+package com.cloume.radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxappApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cloume/radar/WxappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloume.radar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WxappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
