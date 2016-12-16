7da1ae8a65df77451d83723bb73550cbe7af3268
GANG XU <GANG XU>
2016-12-16 18:44:24 +0800

    ngrok测试通过

--- src/main/java/com/cloume/radar/wxapp/WxSdkConfiguration.java
+++ src/main/java/com/cloume/radar/wxapp/WxSdkConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 import com.cloume.radar.wxapp.message.WxClickEventHandler;
 import com.cloume.radar.wxapp.message.WxEventHandler;
--- src/main/java/com/cloume/radar/wxapp/controller/WeixinController.java
+++ src/main/java/com/cloume/radar/wxapp/controller/WeixinController.java
@@ -88,7 +88,9 @@ public class WeixinController {
 	 * @throws WxErrorException
 	 */
 	@ResponseBody @RequestMapping(value = "/jsapisign", method = RequestMethod.POST)
-	public WxJsapiSignature wxJSSdkConfiguration(@RequestBody Map<String, Object> body, HttpServletResponse response) throws WxErrorException{
+	public WxJsapiSignature wxJSSdkConfiguration(
+			@RequestBody Map<String, Object> body, 
+			HttpServletResponse response) throws WxErrorException{
 		/*wx.config({
 		    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
 		    appId: '', // 必填，公众号的唯一标识
